e9ac3db10ac224fb44dd56b1a70f1215f3186404
Patrick-web <pntxall100@gmail.com>
2022-04-13 15:20:52 +0300

    removed service worker

--- src/main.js
+++ src/main.js
@@ -4,16 +4,16 @@ import "./index.css";
 import VueHighlightJS from "vue3-highlightjs";
 import "highlight.js/styles/solarized-light.css";
 
-import { registerSW } from "virtual:pwa-register";
+// import { registerSW } from "virtual:pwa-register";
 
-const updateSW = registerSW({
-  onNeedRefresh() {
-    console.log("Refresh now");
-  },
-  onOfflineReady() {
-    console.log("Offline ready");
-  },
-  strategies: "injectManifest",
-});
+// const updateSW = registerSW({
+//   onNeedRefresh() {
+//     console.log("Refresh now");
+//   },
+//   onOfflineReady() {
+//     console.log("Offline ready");
+//   },
+//   strategies: "injectManifest",
+// });
 
 createApp(App).use(VueHighlightJS).mount("#app");
