8329e234b97ee91938bea33537efd74bc2f5fa3a
Patrick-web <pntxall100@gmail.com>
2022-12-21 08:28:36 +0300

    added remote socket server url

--- src/App.vue
+++ src/App.vue
@@ -218,7 +218,7 @@ export default {
     PasteDeleter,
   },
   async mounted() {
-    const socket = io("ws://localhost:4000")
+    const socket = io("ws://https://pastedeck.deno.dev")
     window.socket = socket;
 
     socket.on("connection-success",(msg)=>{
