172b51b71a241958ae78c596e051b54a035d0ec1
Patrick-web <pntxall100@gmail.com>
2022-12-21 08:40:56 +0300

    fix socket url

--- src/App.vue
+++ src/App.vue
@@ -218,7 +218,7 @@ export default {
     PasteDeleter,
   },
   async mounted() {
-    const socket = io("ws://https://pastedeck.deno.dev")
+    const socket = io("ws://pastedeck.deno.dev")
     window.socket = socket;
 
     socket.on("connection-success",(msg)=>{
