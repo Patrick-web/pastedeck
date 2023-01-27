e6552cbe108a09ca667e932cb9a3bd7a1f89e0c6
Patrick-web <pntxall100@gmail.com>
2023-01-27 20:14:59 +0300

    commented out Vue PWA plugin

--- vite.config.js
+++ vite.config.js
@@ -11,40 +11,40 @@ export default defineConfig({
   },
   plugins: [
     vue(),
-    VitePWA({
-      includeAssets: [
-        "android-chrome-192x192.png",
-        "android-chrome-512x512.png",
-        "apple-touch-icon.png",
-        "favicon-16x16.png",
-        "favicon-32x32.png",
-        "favicon.ico",
-      ],
-      manifest: {
-        name: "Paste Deck",
-        short_name: "PasteDeck",
-        description: "Store and share stuff",
-        theme_color: "#F2E1E1",
-        icons: [
-          {
-            src: "android-chrome-192x192",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "android-chrome-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-          {
-            src: "android-chrome-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable",
-          },
-        ],
-      },
-    }),
+    // VitePWA({
+    //   includeAssets: [
+    //     "android-chrome-192x192.png",
+    //     "android-chrome-512x512.png",
+    //     "apple-touch-icon.png",
+    //     "favicon-16x16.png",
+    //     "favicon-32x32.png",
+    //     "favicon.ico",
+    //   ],
+    //   manifest: {
+    //     name: "Paste Deck",
+    //     short_name: "PasteDeck",
+    //     description: "Store and share stuff",
+    //     theme_color: "#F2E1E1",
+    //     icons: [
+    //       {
+    //         src: "android-chrome-192x192",
+    //         sizes: "192x192",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "android-chrome-512x512.png",
+    //         sizes: "512x512",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "android-chrome-512x512.png",
+    //         sizes: "512x512",
+    //         type: "image/png",
+    //         purpose: "any maskable",
+    //       },
+    //     ],
+    //   },
+    // }),
   ],
   server: {
     host: true,
